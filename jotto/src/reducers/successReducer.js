0686cd9ec8d24032adc275fda851a9148a730b29
Felipe <profissionalf.andre@gmail.com>
2020-10-10 11:08:15 -0300

    feat: add configure store and reducers

---
+++ jotto/src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successArray
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new state
+ */
+export default (state, action) => {
+  return null
+}
